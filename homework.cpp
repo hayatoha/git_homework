efc87cb06b1c63dde262d56326c1a6c9062b3f17
hayatotakeda <falcon-t8864@akane.waseda.jp>
2021-07-24 09:59:56 +0900

    Write a comment out

--- homework.cpp
+++ homework.cpp
@@ -6,24 +6,28 @@ using namespace std;
 
 int main()
 {
+  //変数の設定
     double a = 0.0;
     double b = 0.0;
     double result = 0.0;
     char op;
 
     Calculator c;
+
     while (true)
     {
+      //式の読み取り
       cin >> a >> op >> b;
-      if(op == '/' && b == 0)
+
+      if(op == '/' && b == 0)                         //割り算における0の扱い
       {
-        cout << "Cannot be division by zero" << endl;
+        cout << "Cannot be division by zero" << endl; //エラーメッセージ
         continue;
       }else{
-        result = c.Calculate(a, op, b);
-      }
-        cout << "Result is :" << result << endl; 
+        result = c.Calculate(a, op, b);               //四則演算
+        }
+        
+      cout << "Result is :" << result << endl;        //結果の表示
     }
     return 0;
-
 }
